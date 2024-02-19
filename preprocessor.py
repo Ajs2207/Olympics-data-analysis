56c3c02068df528c85a80ce3c08425ea7e786c96
Abhijeet Samal <abhijeetsml4@gmail.com>
2024-02-20 02:43:38 +0530

    EDA on Olympics Dataset

---
+++ preprocessor.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+def preprocess(df,region_df):
+    
+    #filtering for summer olympics
+    df=df[df['Season']=='Summer']
+
+    #merge with region_df
+    df=df.merge(region_df,on='NOC',how='left')
+
+    #dropping duplicates
+    df.drop_duplicates(inplace=True)
+
+    #one hot encoding medals
+    dummy=pd.get_dummies(df['Medal'],dtype=int)
+
+    df=pd.concat([df,dummy],axis=1)
+
+    return df
